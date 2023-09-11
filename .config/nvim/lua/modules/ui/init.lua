c749f0bf3d01f6c4c132adbe45f54382e8d42562
EdV4H <32019044+EdV4H@users.noreply.github.com>
2023-09-11 15:33:24 +0900

    🎨 feat: add color scheme

--- .config/nvim/lua/modules/ui/init.lua
+++ .config/nvim/lua/modules/ui/init.lua
@@ -1,10 +1,22 @@
 return {
-  'folke/noice.nvim',
-  dependencies = {
-    'MunifTanjim/nui.nvim',
-    'rcarriga/nvim-notify',
+  -- rich ui
+  {
+    'folke/noice.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+    config = function()
+      require('noice').setup({})
+    end,
   },
-  config = function()
-    require('noice').setup({})
-  end,
+  -- automatically syncs terminal background and cursor with any neovim colorscheme.
+  {
+    "typicode/bg.nvim",
+    lazy = false,
+  },
+  -- color scheme
+  {
+    'sainnhe/everforest',
+  },
 }
