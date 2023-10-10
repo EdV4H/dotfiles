82277934bc8ad740d382d53070812bef25fdd965
EdV4H <32019044+EdV4H@users.noreply.github.com>
2023-10-10 13:36:58 +0900

    ✨ feat: add lspsaga trouble

--- .config/nvim/lua/modules/completion/init.lua
+++ .config/nvim/lua/modules/completion/init.lua
@@ -14,4 +14,34 @@ return {
   {
     'neovim/nvim-lspconfig', config = conf.nvim_lsp,
   },
+  {
+    'glepnir/lspsaga.nvim',
+    event = 'LspAttach',
+    dev = false,
+    config = conf.lspsaga,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
+  {
+    'folke/trouble.nvim',
+    config = conf.trouble,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      -- Disable copilot suggestion
+      -- SEE: https://github.com/zbirenbaum/copilot-cmp#setup
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
 }
