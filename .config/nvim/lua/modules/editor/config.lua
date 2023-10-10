e1898dfce7dad235bbe22c83f8357bce4a144c57
EdV4H <32019044+EdV4H@users.noreply.github.com>
2023-10-10 13:37:22 +0900

    🔧 chore: enable highlight default

--- .config/nvim/lua/modules/editor/config.lua
+++ .config/nvim/lua/modules/editor/config.lua
@@ -3,6 +3,10 @@ local config = {}
 function config.nvim_treesitter()
   return require('nvim-treesitter.configs').setup({
     ensure_installed = 'all',
+    highlight = {
+      enable = true,
+      disable = {},
+    }
   })
 end
 
