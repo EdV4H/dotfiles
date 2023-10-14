59e9929e180b12e3b26b2ce8fdda3dfa8e8caab4
EdV4H <32019044+EdV4H@users.noreply.github.com>
2023-10-14 18:44:18 +0900

    ✨ feat: add formatter

--- .config/nvim/lua/modules/completion/config.lua
+++ .config/nvim/lua/modules/completion/config.lua
@@ -1,36 +1,71 @@
 local config = {}
 
 function config.mason()
-  require('mason').setup({
+  require("mason").setup({
     ui = {
       icons = {
         package_installed = "✓",
         package_pending = "➜",
-        package_uninstalled = "✗"
-      }
-    }
+        package_uninstalled = "✗",
+      },
+    },
   })
 end
 
 function config.mason_lspconfig()
-  local lspconfig = require('lspconfig')
-  local mason_lspconfig = require('mason-lspconfig')
+  local lspconfig = require("lspconfig")
+  local mason_lspconfig = require("mason-lspconfig")
   mason_lspconfig.setup({})
   mason_lspconfig.setup_handlers({
     function(server_name)
-      lspconfig[server_name].setup({})
+      local opts = {}
+      opts.on_attach = function(_, bufnr)
+        local bufopts = { silent = true, noremap = true, buffer = bufnr }
+
+        vim.keymap.set("n", "<S-k>", "<cmd>Lspsaga hover_doc<cr>", bufopts)
+        vim.keymap.set("n", "<leader>ac", "<cmd>Lspsaga code_action<cr>", bufopts)
+      end
+      lspconfig[server_name].setup(opts)
     end,
   })
 end
 
+function config.null_ls()
+  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+  local null_ls = require("null-ls")
+  null_ls.setup({
+    sources = {
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier.with({
+        prefer_local = "node_modules/.bin",
+      }),
+      null_ls.builtins.diagnostics.eslint_d,
+    },
+    on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end,
+        })
+      end
+    end,
+  })
+end
+
+function config.mason_null_ls() end
+
 function config.nvim_lsp() end
 
 function config.lspsaga()
-  require('lspsaga').setup({})
+  require("lspsaga").setup({})
 end
 
 function config.trouble()
-  require('trouble').setup({})
+  require("trouble").setup({})
 end
 
 function config.copilot()
@@ -43,17 +78,19 @@ function config.copilot()
 end
 
 function config.nvim_cmp()
-  local cmp = require('cmp')
-  local lspkind = require('lspkind')
+  local cmp = require("cmp")
+  local lspkind = require("lspkind")
 
   -- SEE: https://github.com/zbirenbaum/copilot-cmp#tab-completion-configuration-highly-recommended
   local has_words_before = function()
-    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+      return false
+    end
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
   end
 
-  require('copilot_cmp').setup()
+  require("copilot_cmp").setup()
   lspkind.init({
     symbol_map = {
       Copilot = "",
@@ -63,11 +100,11 @@ function config.nvim_cmp()
   cmp.setup({
     preselect = cmp.PreselectMode.Item,
     sources = {
-      { name = 'copilot' },
-      { name = 'nvim_lsp' },
-      { name = 'nvim_lsp_signature_help' },
-      { name = 'buffer' },
-      { name = 'path' },
+      { name = "copilot" },
+      { name = "nvim_lsp" },
+      { name = "nvim_lsp_signature_help" },
+      { name = "buffer" },
+      { name = "path" },
     },
     window = {
       completion = cmp.config.window.bordered(),
@@ -77,11 +114,11 @@ function config.nvim_cmp()
       format = lspkind.cmp_format({ with_text = true, maxwidth = 50 }),
     },
     mapping = {
-      ['<CR>'] = cmp.mapping.confirm({
+      ["<CR>"] = cmp.mapping.confirm({
         behavior = cmp.ConfirmBehavior.Replace,
         select = false,
       }),
-      ['<Tab>'] = vim.schedule_wrap(function(fallback)
+      ["<Tab>"] = vim.schedule_wrap(function(fallback)
         if cmp.visible() and has_words_before() then
           cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
         else
@@ -97,56 +134,55 @@ function config.nvim_cmp()
   })
 
   -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
+  cmp.setup.cmdline({ "/", "?" }, {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
-      { name = 'nvim_lsp_signature_help' },
-      { name = 'buffer' },
+      { name = "nvim_lsp_signature_help" },
+      { name = "buffer" },
     },
   })
 
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
+  cmp.setup.cmdline(":", {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
-      { name = 'path' },
-    },
-    {
+      { name = "path" },
+    }, {
       {
-        name = 'cmdline',
+        name = "cmdline",
         option = {
-          ignore_cmds = { 'Man', '!' }
-        }
+          ignore_cmds = { "Man", "!" },
+        },
       },
       {
-        name = 'cmdline_history',
+        name = "cmdline_history",
         option = {
-          ignore_cmds = { 'Man', '!' }
-        }
+          ignore_cmds = { "Man", "!" },
+        },
       },
-    })
+    }),
   })
 end
 
 function config.lua_snip()
-  local ls = require('luasnip')
-  local types = require('luasnip.util.types')
+  local ls = require("luasnip")
+  local types = require("luasnip.util.types")
   ls.config.set_config({
     history = true,
     enable_autosnippets = true,
-    updateevents = 'TextChanged,TextChangedI',
+    updateevents = "TextChanged,TextChangedI",
     ext_opts = {
       [types.choiceNode] = {
         active = {
-          virt_text = { { '<- choiceNode', 'Comment' } },
+          virt_text = { { "<- choiceNode", "Comment" } },
         },
       },
     },
   })
-  require('luasnip.loaders.from_lua').lazy_load({ paths = vim.fn.stdpath('config') .. '/snippets' })
-  require('luasnip.loaders.from_vscode').lazy_load()
-  require('luasnip.loaders.from_vscode').lazy_load({
-    paths = { './snippets/' },
+  require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/snippets" })
+  require("luasnip.loaders.from_vscode").lazy_load()
+  require("luasnip.loaders.from_vscode").lazy_load({
+    paths = { "./snippets/" },
   })
 end
 
--- .config/nvim/lua/modules/completion/init.lua
+++ .config/nvim/lua/modules/completion/init.lua
@@ -1,58 +1,68 @@
-local conf = require('modules.completion.config')
+local conf = require("modules.completion.config")
 
 return {
   {
-    'williamboman/mason.nvim',
+    "williamboman/mason.nvim",
     event = { "BufReadPre", "VimEnter" },
     build = ":MasonUpdate",
     config = conf.mason,
   },
   {
-    'williamboman/mason-lspconfig.nvim',
+    "williamboman/mason-lspconfig.nvim",
     config = conf.mason_lspconfig,
   },
   {
-    'neovim/nvim-lspconfig', config = conf.nvim_lsp,
+    "nvimtools/none-ls.nvim",
+    event = { "BufReadPre", "VimEnter" },
+    config = conf.null_ls,
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
   },
   {
-    'glepnir/lspsaga.nvim',
-    event = 'LspAttach',
+    "neovim/nvim-lspconfig",
+    config = conf.nvim_lsp,
+  },
+  {
+    "glepnir/lspsaga.nvim",
+    event = "LspAttach",
     dev = false,
     config = conf.lspsaga,
     dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons',
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
     },
   },
   {
-    'folke/trouble.nvim',
+    "folke/trouble.nvim",
     config = conf.trouble,
     dependencies = {
-      'nvim-tree/nvim-web-devicons',
+      "nvim-tree/nvim-web-devicons",
     },
   },
   {
-    'zbirenbaum/copilot.lua',
+    "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
     config = conf.copilot,
   },
   {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     config = conf.nvim_cmp,
     dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'dmitmel/cmp-cmdline-history',
-      'zbirenbaum/copilot.lua',
-      'zbirenbaum/copilot-cmp',
-      'saadparwaiz1/cmp_luasnip',
-      'onsails/lspkind-nvim',
-      'hrsh7th/vim-vsnip',
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "dmitmel/cmp-cmdline-history",
+      "zbirenbaum/copilot.lua",
+      "zbirenbaum/copilot-cmp",
+      "saadparwaiz1/cmp_luasnip",
+      "onsails/lspkind-nvim",
+      "hrsh7th/vim-vsnip",
     },
   },
-  { 'L3MON4D3/LuaSnip', event = 'InsertCharPre', config = conf.lua_snip }
+  { "L3MON4D3/LuaSnip", event = "InsertCharPre", config = conf.lua_snip },
 }
