9fbba497315369688a261fe7b5bb3e3c4ea51d8f
EdV4H <circlemountain.ym@gmail.com>
2025-07-03 20:34:57 +0900

    :tada: feat: add initial flake files

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  };
+
+  outputs = { self, nixpkgs, neovim-nightly-overlay }: 
+    let
+        system = "aarch64-darwin";
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin.extend (
+            neovim-nightly-overlay.overlays.default
+        );
+    in
+  {
+    packages.${system}.my-packages = pkgs.buildEnv {
+        name = "my-packages-list";
+        paths = with pkgs;
+            [
+                git
+                curl
+                neovim
+            ];
+    };
+  };
+}
