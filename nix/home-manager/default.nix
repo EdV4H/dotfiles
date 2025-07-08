3b49bbf0b0c529949f3f38828cc673ce53e599b9
EdV4H <circlemountain.ym@gmail.com>
2025-07-08 16:58:12 +0900

    :wrench: feat: add zsh alias

--- nix/home-manager/default.nix
+++ nix/home-manager/default.nix
@@ -32,9 +32,18 @@ in
       neovim
       docker
       lazygit
+      claude-code
     ];
   };
 
   programs.home-manager.enable = true;
   programs.wezterm = import ./programs/wezterm/default.nix;
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      lg = "lazygit";
+      la = "ls -a";
+    };
+  };
 }
